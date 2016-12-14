9ca1d197ea785e80d001bc5b5ddcdf67e9fb4342
Jessa Wright <jessa.wright@gmail.com>
2016-12-14 11:56:38 -0600

    more tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
-var thing = {
+var webpackSettings = {
   entry: './app/app.jsx',
   output: {
     path: './site/js',
     filename: 'app.js'
-  }
+  },
+  module: {
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }
+  ]
+}
+
 
 
 }
 
-module.exports = thing
+module.exports = webpackSettings
